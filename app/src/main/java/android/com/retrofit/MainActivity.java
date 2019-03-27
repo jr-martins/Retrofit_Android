27246b1d183355c103154c3ffa223d0364fcf159
Junior Martins <valter.martins@metasix.com.br>
2019-03-27 20:41:20 -0300

    RetroFit

---
+++ app/src/main/java/android/com/retrofit/MainActivity.java
@@ -0,0 +1,30 @@
+package android.com.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText cep = findViewById(R.id.etMain_cep);
+        final TextView resposta = findViewById(R.id.etMain_resposta);
+
+        Button btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+        btnBuscarCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+
+            }
+        });
+    }
+}
